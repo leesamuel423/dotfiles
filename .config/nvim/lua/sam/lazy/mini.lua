7319c7c40ce48e19c09f588bcb05b9486bb4fdd0
Samuel Lee <leesamuel423@gmail.com>
2025-04-23 21:02:04 -0400

    chore: nvim config + zshrc configs

---
+++ .config/nvim/lua/sam/lazy/mini.lua
@@ -0,0 +1,20 @@
+local M = {
+    "echasnovski/mini.nvim",
+    version = "*",
+}
+
+function M.config()
+    local spec_treesitter = require("mini.ai").gen_spec.treesitter
+
+    -- Load mini.nvim modules with their default configurations
+
+    require("mini.ai").setup()
+    require("mini.pairs").setup()
+    require("mini.statusline").setup()
+    require("mini.surround").setup()
+    require("mini.align").setup()
+end
+
+return M
+
+
---
+++ .config/nvim/lua/sam/lazy/showkeys.lua
@@ -0,0 +1,16 @@
+local M = {
+	"nvzone/showkeys",
+}
+
+function M.config()
+	require("showkeys").setup({
+		dev = true,
+		maxkeys = 5,
+		-- excluded_modes = { "i" },
+		position = "top-right",
+	})
+end
+
+return M
+
+
